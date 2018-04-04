6f926d8268f952e895b072e9f7e811fb81d47712
toChaim <toChaim@gmail.com>
2018-04-04 12:32:52 -0700

    building server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import notFoundErr from './error/not_found';
+
 const PORT = process.env.PORT || 3000;
 
 const api = express();
+
+// temp root route
+api.get('/',(req,res,next)=>{
+    return res.json({message: "This is a very dull page. What is on the next page?"});
+});
+
+// catch unfound routes
+api.use(notFoundErr);
 
 api.listen(PORT, err => {
     if(err){ console.log(err); }
